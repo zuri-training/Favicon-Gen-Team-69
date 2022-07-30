dbfd5d2b355d45dc65af5281b2ab021e9d2378ca
Alade Akeem <aladetomiwa31@gmail.com>
2022-07-30 15:34:58 +0100

    made modifications to the settings and urls file

--- Backend/favicon_generator/settings.py
+++ Backend/favicon_generator/settings.py
@@ -39,8 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'knox'
-    'rest_framework_simplejwt',
+    'knox',
+    # 'rest_framework_simplejwt',
     'cloudinary_storage',
     'cloudinary',
     'favicon',
