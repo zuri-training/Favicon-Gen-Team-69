85a123d4903c0e3aa228574cc895691ae71c86e6
Elisha Bagaiya <105201298+Bagais@users.noreply.github.com>
2022-08-04 23:49:14 +0100

    create helper functions
    created functions for favicon generation.
    1. gen _fav
    2. gen_all_fav

--- Backend/favicon/helpers.py
+++ Backend/favicon/helpers.py
@@ -1,12 +1,29 @@
-def generate_fav(image, size):
+from PIL import Image
+from io import BytesIO
+from django.core.files.base import ContentFile
+from django.core.files.uploadedfile import InMemoryUploadedFile
+from .models import Icon
+
+def generate_fav(image, size, favicon_id):
 	if size == "favicon":
 		resized_img = image.resize((64,64))
-		resized_img.save('favicon.ico')	#convert to blob for saving to database
+		img_name = "favicon"
 	else:
 		resized_img = image.resize(size)
 		img_name = f"{size[0]}x{size[0]}"
-		resized_img.save(f"icon{img_name}.png")	#convert to blob for saving to database
+	blob = BytesIO()
+	resized_img.save(fp=blob, format="PNG")
+	blob_val = blob.getvalue()
+	file_image = ContentFile(blob_val)
+	icon_file = InMemoryUploadedFile(file_image, None, 'foo.png', 'image/png', file_image.tell, None)
+	
+	my_icon = Icon(favicon=favicon_id)
+	upload_icon = my_icon.icon
+	upload_icon.save(img_name, icon_file)
 
-def generate_all_fav(image, sizes):
+	my_icon.save()
+
+def generate_all_fav(image, sizes, favicon_id):
+	pillow_image = Image.open(image)
 	for size in ico_sizes:
-		generate_fav(image, size)
+		generate_fav(pillow_image, size, favicon_id)
