1a41ac81d23139f732e0da5c640b1b754eec9f30
CM <etchiedaniel1@gmail.com>
2022-08-11 02:32:30 +0100

    Migrated Database to PosgreSQL and renamed all endpoints properly

--- Backend/favicon/urls.py
+++ Backend/favicon/urls.py
@@ -5,7 +5,7 @@ from favicon import views
 urlpatterns = [
     path('favicons/', views.FaviconListView.as_view(), name='favicon_list'),
     path('favicon/<int:pk>/', views.UpdateFaviconView.as_view(), name='favicon_update'),
-    path('favicon_generate/', views.CreateFaviconView.as_view(), name='favicon_generate'),
+    path('generate_favicon/', views.CreateFaviconView.as_view(), name='favicon_generate'),
     path('text_preview/', views.TextFaviPreview.as_view(), name='text_preview'),
     path('emoji_preview/', views.EmojiFaviPreview.as_view(), name='emoji_preview'),
 
