9d89b4807f61c8d35101c12607456b904cfc78ba
EyimofeT <mofii4u@gmail.com>
2022-08-14 23:40:04 +0100

    Adding JS Color Picker

--- Frontend/js/texttofavicon.js
+++ Frontend/js/texttofavicon.js
@@ -198,3 +198,55 @@ function hexToRgb(hex) {
     ] : null;
 
 }
+
+// color picker 
+// default_color='#000000'
+const pickr = Pickr.create({
+    el: '.color-picker',
+    theme: 'monolith', // or 'monolith', or 'nano'
+    // default:default_color,
+
+    swatches: [
+        'rgba(244, 67, 54, 1)',
+        'rgba(233, 30, 99, 0.95)',
+        'rgba(156, 39, 176, 0.9)',
+        'rgba(103, 58, 183, 0.85)',
+        'rgba(63, 81, 181, 0.8)',
+        'rgba(33, 150, 243, 0.75)',
+        'rgba(3, 169, 244, 0.7)',
+        'rgba(0, 188, 212, 0.7)',
+        'rgba(0, 150, 136, 0.75)',
+        'rgba(76, 175, 80, 0.8)',
+        'rgba(139, 195, 74, 0.85)',
+        'rgba(205, 220, 57, 0.9)',
+        'rgba(255, 235, 59, 0.95)',
+        'rgba(255, 193, 7, 1)'
+    ],
+
+    components: {
+
+        // Main components
+        preview: true,
+        opacity: true,
+        hue: true,
+
+        // Input / output Options
+        interaction: {
+            // hex: true,
+            // rgba: true,
+            // hsla: true,
+            // hsva: true,
+            // cmyk: true,
+            // input: true,
+            clear: true,
+            save: true
+        }
+    }
+});
+pickr.on('change', (color, source, instance) => {
+    // console.log('Event: "change"', color, source, instance);
+    // default_color=color
+    
+}).on('save', (color, instance) => {
+    console.log(color.toHEXA().toString())
+});
